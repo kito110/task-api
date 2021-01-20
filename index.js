8bd7c014e5b04a3ffa53bb3e61c89268115d6576
kito110 <77045348+kito110@users.noreply.github.com>
2021-01-20 09:51:08 +0530

    Added mongo connection in index file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require("express");
+var mongoose = require("mongoose");
 require("dotenv").config();
 var port = process.env.PORT;
+var db_url = process.env.DB_URL;
 var app = express();
 var bodyParser = require('body-parser');
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 
-app.listen(port,function(req,res)
+app.listen(port,async(req,res)=>
 {
+await mongoose.connect(db_url)
 console.log(`App started at ${port}`);
 });
 
