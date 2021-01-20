e61447e3ceab51f60214e0e1f961289b31c0ca74
kito110 <77045348+kito110@users.noreply.github.com>
2021-01-20 09:53:31 +0530

    removed the mongo connection

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,9 +1,6 @@
 var mongoose=require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.DB_URL,function(err){
-if(err)throw err;
-console.log("DB connected");
-});
+
 var userSchema=new mongoose.Schema({username : String,firstName : String,lastName : String,email : String,status : Number,thumbnail : String,role : String,lastSigninDate : Date,createdAt : Date,lastModified : Date,permissions : Array,contact : Number});
 var usermodel=mongoose.model("user",userSchema,"Users");
 
